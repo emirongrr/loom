aa2bbd1d6b4a5a33bdcb971046f36d848c9d3bb6
emirongrr <80769968+emirongrr@users.noreply.github.com>
2026-06-19 12:30:52 +0300

    test(formal): expand recovery and freeze invariants (#46)

--- test/formal/LoomAccountRecoveryFormal.t.sol
+++ test/formal/LoomAccountRecoveryFormal.t.sol
@@ -3,9 +3,10 @@ pragma solidity 0.8.35;
 
 import {LoomAccount} from "../../src/account/LoomAccount.sol";
 import {RecoveryManager} from "../../src/recovery/RecoveryManager.sol";
+import {ExecutionLib} from "../../src/libraries/ExecutionLib.sol";
 import {ModuleType} from "../../src/libraries/ModuleType.sol";
 import {MockValidator} from "../mocks/MockValidator.sol";
-import {FormalAccountBase, FormalGuardianVerifier} from "./FormalHelpers.sol";
+import {FormalAccountBase, FormalGuardianVerifier, FormalTarget} from "./FormalHelpers.sol";
 
 contract LoomAccountRecoveryFormal is FormalAccountBase {
     function check_recoveryCannotExecuteBeforeDelayAndReplacesCompleteSet() public {
@@ -41,5 +42,58 @@ contract LoomAccountRecoveryFormal is FormalAccountBase {
         assert(account.isModuleInstalled(ModuleType.VALIDATOR, address(newValidator)));
         assert(!account.isModuleInstalled(ModuleType.VALIDATOR, address(oldValidator)));
         assert(account.guardianRoot() == newGuardianRoot);
+    }
+
+    function check_frozenAccountCanCancelRecoveryButNotOrdinarySchedule() public {
+        RecoveryManager recovery = new RecoveryManager();
+        FormalGuardianVerifier verifier = new FormalGuardianVerifier();
+        MockValidator oldValidator = new MockValidator();
+        MockValidator newValidator = new MockValidator();
+        bytes32 keyCommitment = keccak256("key");
+        bytes32 salt = keccak256("salt");
+        bytes32 leaf = keccak256(abi.encode(address(verifier), address(verifier).codehash, keyCommitment, salt));
+        LoomAccount.ModuleInit[] memory modules = new LoomAccount.ModuleInit[](2);
+        modules[0] = LoomAccount.ModuleInit(ModuleType.VALIDATOR, address(oldValidator), "");
+        modules[1] = LoomAccount.ModuleInit(ModuleType.RECOVERY, address(recovery), "");
+        LoomAccount account = new LoomAccount(address(this), leaf, 1, keccak256("config"), modules);
+
+        FormalTarget target = new FormalTarget();
+        bytes memory targetCall = abi.encodeCall(FormalTarget.setValue, (uint256(7)));
+        bytes memory scheduleTarget =
+            abi.encodeCall(LoomAccount.scheduleCall, (address(target), 0, targetCall, account.MIN_HIGH_RISK_DELAY()));
+        account.execute(bytes32(0), abi.encode(ExecutionLib.Execution(address(account), 0, scheduleTarget)));
+        bytes32 scheduledId = keccak256(abi.encode(address(target), uint256(0), targetCall, account.configVersion()));
+
+        address[] memory oldValidators = new address[](1);
+        oldValidators[0] = address(oldValidator);
+        RecoveryManager.GuardianApproval[] memory approvals = new RecoveryManager.GuardianApproval[](1);
+        approvals[0] = RecoveryManager.GuardianApproval(address(verifier), keyCommitment, salt, "", new bytes32[](0));
+        recovery.proposeRecovery(
+            address(account),
+            oldValidators,
+            address(newValidator),
+            keccak256(bytes("")),
+            keccak256("new-guardians"),
+            1,
+            approvals
+        );
+
+        account.freeze(address(verifier), keyCommitment, salt, new bytes32[](0), "");
+        bytes memory cancelRecovery = abi.encodeCall(RecoveryManager.cancelRecovery, (address(account)));
+        account.execute(bytes32(0), abi.encode(ExecutionLib.Execution(address(recovery), 0, cancelRecovery)));
+        (,,,,, uint48 readyAt,,,) = recovery.pendingRecoveries(address(account));
+        assert(readyAt == 0);
+        assert(recovery.recoveryNonces(address(account)) == 1);
+
+        bytes memory cancelScheduled = abi.encodeCall(LoomAccount.cancelScheduled, (scheduledId));
+        (bool ordinaryCancel,) = address(account)
+            .call(
+                abi.encodeCall(
+                    LoomAccount.execute,
+                    (bytes32(0), abi.encode(ExecutionLib.Execution(address(account), 0, cancelScheduled)))
+                )
+            );
+        assert(!ordinaryCancel);
+        assert(account.scheduledOperations(scheduledId) != 0);
     }
 }
